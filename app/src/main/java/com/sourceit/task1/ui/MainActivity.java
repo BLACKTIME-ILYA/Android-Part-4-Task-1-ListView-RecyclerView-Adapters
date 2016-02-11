84d18b193ce98a61f750e69cd90a33266b245fa4
User <putnin.ilya@gmail.com>
2016-02-11 08:05:49 +0200

    init

--- app/src/main/java/com/sourceit/task1/ui/MainActivity.java
+++ app/src/main/java/com/sourceit/task1/ui/MainActivity.java
@@ -1,16 +1,11 @@
 package com.sourceit.task1.ui;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ImageView;
-import android.widget.ListView;
-import android.widget.TextView;
 
 import com.sourceit.task1.R;
 import com.sourceit.task1.model.Contact;
@@ -33,50 +28,15 @@ public class MainActivity extends AppCompatActivity {
             contacts.add(new Contact("Name" + i, "Email@" + i, "Adress " + i, R.drawable.ic_contact_phone_black_48dp));
         }
 
-        ListView contact_list = (ListView) findViewById(R.id.contact_list);
-        contact_list.setAdapter(new MyAdapter(this, contacts));
-
-        contact_list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        RecyclerView contact_list = (RecyclerView) findViewById(R.id.contact_list);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        contact_list.setLayoutManager(layoutManager);
+        contact_list.setAdapter(new RecyclerViewAdapter(contacts, new OnItemClickWatcher() {
             @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            public void onItemClick(View v, int position, Object item) {
                 openInformation(position);
             }
-        });
-    }
-
-
-    private class MyAdapter extends ArrayAdapter<Contact> {
-        public MyAdapter(Context context, ArrayList<Contact> objects) {
-            super(context, R.layout.contact, objects);
-        }
-
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-            ViewHolder holder;
-            View rowView = convertView;
-
-            if (rowView == null) {
-                rowView = getLayoutInflater().inflate(R.layout.contact, parent, false);
-                holder = new ViewHolder();
-                holder.name = (TextView) rowView.findViewById(R.id.contact_name);
-                holder.email = (TextView) rowView.findViewById(R.id.contact_email);
-                holder.image = (ImageView) rowView.findViewById(R.id.contact_icon);
-                rowView.setTag(holder);
-            } else holder = (ViewHolder) rowView.getTag();
-
-            Contact contact = getItem(position);
-            holder.name.setText(contact.getName());
-            holder.email.setText(contact.getEmail());
-            holder.image.setImageResource(contact.getImage());
-
-            return rowView;
-        }
-
-        class ViewHolder {
-            public TextView name;
-            public TextView email;
-            public ImageView image;
-        }
+        }));
     }
 
     private void openInformation(int position) {
